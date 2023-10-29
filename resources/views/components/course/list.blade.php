b304337b6dd11604fc5c914b2634ef38c76422cf
khfii <88762395+khfii@users.noreply.github.com>
2023-10-29 21:30:52 +0800

    Course finish up

--- resources/views/components/course/list.blade.php
+++ resources/views/components/course/list.blade.php
@@ -1,5 +1,60 @@
-<div class="flex flex-col lg:px-2 py-20">
-    <div class="heading">
-        <h2 class="text-2xl font-bold text-coklat uppercase text-center">courses</h2>
+<div class="flex flex-col lg:px-20 py-20">
+    <div class="heading text-center flex flex-col gap-y-2">
+        <h2 class="text-2xl font-bold text-coklat uppercase">courses</h2>
+        <p class="text-base text-coklat">Join thousands of others who have taken my online courses to learn some of life’s most important skills and mindsets.</p>
+        <div class="flex flex-wrap justify-center gap-10 mt-5">
+            <div class="basis-[30%]">
+                <div class="flex flex-col gap-y-4 justify-center items-center">
+                    <div class="avatar">
+                        <div class="w-24 rounded-full">
+                            <img src="{{ asset('icon/freedom.png') }}" alt="">
+                        </div>
+                    </div>
+
+                 <div class="w-[60%] space-y-2">
+                       <h2 class="text-lg font-bold text-coklat">Finding and Making Our Happines</h2>
+                    <p class="text-sm text-coklat text-left">Get a solid grasp on the principles that make for a successful dating life and fulfilling relationships.</p>
+                </div>
+
+                <button class="badge badge-lg w-36 h-8 bg-kuning text-base font-bold uppercase p-2">learn more</button>
+
+                </div>
+            </div>
+            <div class="basis-[30%]">
+                <div class="flex flex-col gap-y-4 justify-center items-center">
+                    <div class="avatar">
+                        <div class="w-24 rounded-full">
+                            <img src="{{ asset('icon/sad.png') }}" alt="">
+                        </div>
+                    </div>
+
+                 <div class="w-[60%] space-y-2">
+                       <h2 class="text-lg font-bold text-coklat">Dont Ever Sad for Something Useles</h2>
+                    <p class="text-sm text-coklat text-left">Get a solid grasp on the principles that make for a successful dating life and fulfilling relationships.</p>
+                </div>
+
+                <button class="badge badge-lg w-36 h-8 bg-kuning text-base font-bold uppercase p-2">learn more</button>
+
+                </div>
+            </div>
+            <div class="basis-[30%]">
+                <div class="flex flex-col gap-y-4 justify-center items-center">
+                    <div class="avatar">
+                        <div class="w-24 rounded-full">
+                            <img src="{{ asset('icon/relationship.png') }}" alt="">
+                        </div>
+                    </div>
+
+                 <div class="w-[60%] space-y-2">
+                       <h2 class="text-lg font-bold text-coklat">Finding and Making Good Relationship </h2>
+                    <p class="text-sm text-coklat text-left">Get a solid grasp on the principles that make for a successful dating life and fulfilling relationships.</p>
+                </div>
+
+                <button class="badge badge-lg w-36 h-8 bg-kuning text-base font-bold uppercase p-2">learn more</button>
+
+                </div>
+            </div>
+
+        </div>
     </div>
 </div>
